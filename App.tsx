fee454dbb8addf487779f26ea080b801ccdf5571
OjuaraCoder <wesley.fernandes.araujo@gmail.com>
2025-02-19 16:50:52 -0300

    ajustes para build

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from './src/routes';
-import { AuthProvider } from './src/contexts/AuthContext';
-
 import { AuthProvider } from './src/contexts/AuthContext';
 
 export default function App() {
